1514a6964ae4df508f4c7e5b15310363a33d6b0d
adrianbinah <adrian.navarro@binah.mx>
2012-02-01 15:29:19 -0600

    iniciode sistema hackerhub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerhub::Application.config.secret_token = 'c29e22227ece53153850c9cfeee072db51d6fc06134752c6e9ad304686981de96be6faca52920d4cd5fb80896322642b39ce21d0214b5e58f06acfea81ba8604'
